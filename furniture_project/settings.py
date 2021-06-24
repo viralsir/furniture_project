8823988eedf7d13844d429056cbebace12657790
viralsir <viralsir2018@gmail.com>
2021-06-24 16:25:11 +0530

    user authentication and theme setup is completed.

--- furniture_project/settings.py
+++ furniture_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK='bootstrap4'
+LOGIN_REDIRECT_URL='user-home'
--- furniture_project/urls.py
+++ furniture_project/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
+    path("",LoginView.as_view(template_name='users/login.html'),name="login"),
+    path("logout",LogoutView.as_view(template_name='users/logout.html'),name="logout"),
     path('admin/', admin.site.urls),
+    path("users/",include("users.urls")),
+
 ]
